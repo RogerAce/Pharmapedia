acd2007785880fe8a435aadeb527955e6dee9ff7
RogerAce <roopam.panwar@gmail.com>
2018-03-21 19:23:56 +0530

    Revert "make it better"
    This reverts commit efb4006758b3c293b0a97656a0333f2b0ad8bb69.

--- medapi/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-"""admin.site.register(medicine)
-admin.site.register(Gname)
-admin.site.register(Bname)"""
-
-# Register your models here.
--- medapi/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MedapiConfig(AppConfig):
-    name = 'medapi'
