efb4006758b3c293b0a97656a0333f2b0ad8bb69
RogerAce <roopam.panwar@gmail.com>
2018-03-21 18:32:10 +0530

    make it better

---
+++ medapi/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+
+"""admin.site.register(medicine)
+admin.site.register(Gname)
+admin.site.register(Bname)"""
+
+# Register your models here.
